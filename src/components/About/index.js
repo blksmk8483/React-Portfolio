ab8fcc42b199435827367e2fbe089b518a1298e0
Brandon Knight <knightbrandon1984@gmail.com>
2022-12-04 22:03:30 -0600

    added About Me

---
+++ src/components/About/index.js
@@ -0,0 +1,25 @@
+import './index.scss'
+
+const About = () => {
+    
+    return (
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                About Me:
+                </h1>
+                <p>
+                stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me
+                </p>
+                <p>
+                stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me
+                </p>
+                <p>
+                stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
