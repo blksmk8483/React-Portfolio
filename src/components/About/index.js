906a9da13d1f075aa7baa8a992be2d0239d3d995
Brandon Knight <knightbrandon1984@gmail.com>
2022-12-05 23:23:24 -0600

    updated the ABOUT ME page and changed some styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,15 +9,25 @@ const About = () => {
                 <h1>
                 About Me:
                 </h1>
-                <p>
-                stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me
+                <p className='about-text'>
+                I am here for the challenge! I pride myself in taking on challenges, solving them, and trying to be creative in the process. 
                 </p>
-                <p>
-                stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me
+                <p className='about-text'>
+                Soon to be (Dec. 13) graduate from the University of Texas Austin Full Stack Coding Bootcamp. Experience with React, JavaScript, HTML, CSS, MongoDB, Bootstrap and more... I am excited to start a new career as Full-Stack Web Developer.
                 </p>
-                <p>
-                stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me  stuff about me
+                <p className='about-text'>
+                Prior to web development I was Director of Percussion in a large 6A high school. I was part of a team directors that collectively led our program to many great successes in the Fine Arts World. 
                 </p>
+                <p className='about-text'>
+                My prior skills include Teaching, Music Technology (Finale, audio design), Microsoft Office, Classroom Management, Public Speaking, Public Engagement, Communication, Team Collaboration across multiple platforms.
+                </p>
+                <p className='about-text'>
+                I have a Bachelor of Science in Music Education (EC-12) with performance emphasis.
+                </p>
+                <p className='about-text'>
+                I also started my own business, Lavender Cove Farm. We sell locally and online to customers in the Central, TX area. We help educate the central Texas area on growing not only lavender but many different herbs. Maitaining inventory, stocking multiple outlets that we sell too, fulfilling online orders, and keeping a strong relationshp between our customers are some of my small business skills.
+                </p>
+              
             </div>
             <div className='about-right-page'>
                 <img className='profile-pic' src={ProfilePhoto} alt="profilePic" />
