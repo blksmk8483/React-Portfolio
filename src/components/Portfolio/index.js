edaf8a9992101805167c7ab177cda3b3f878393a
Brandon Knight <knightbrandon1984@gmail.com>
2022-12-07 00:14:09 -0600

    added styling to the portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -14,7 +14,15 @@ const renderPortfolio = (portfolio) => {
                             <img
                             src={port.cover}
                             className="portfolio-image"
-                            alt="garden2" />
+                            alt="portfolio" />
+                            <div className="content">
+                                <p className="title">{port.title}</p>
+                                <h4 className="description">{port.description}</h4>
+                                <button
+                                    className="btn"
+                                    onClick={() => window.open(port.url)}
+                                    >VIEW</button>
+                                </div>
                         </div>
                     )
                 })
