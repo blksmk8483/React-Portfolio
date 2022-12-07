ff94e37bc96ddd9653409ffb7c66d205e251dd33
Brandon Knight <knightbrandon1984@gmail.com>
2022-12-06 23:22:28 -0600

    I have an image in my portfolio, made a database for projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,12 +1,32 @@
 import React from "react";
 import './index.scss';
-
+import portfolioData from '../../data/portfolio.json';
 
 const Portfolio = () => {
+
+const renderPortfolio = (portfolio) => {
+    return (
+        <div className="images-container">
+            {
+                portfolio.map((port, index) => {
+                    return (
+                        <div className="image-box" key={index}>
+                            <img
+                            src={port.cover}
+                            className="portfolio-image"
+                            alt="garden2" />
+                        </div>
+                    )
+                })
+            }
+        </div>
+    );
+}
+
     return (
         <div className='container portfolio-page'>
             <h1 className="page-title">Portfolio</h1>
-            
+        <div>{renderPortfolio(portfolioData.portfolio)}</div>
         </div>
     )
 }
