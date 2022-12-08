8d56b5f00f0856375f325f5d234900efdc72175d
Brandon Knight <knightbrandon1984@gmail.com>
2022-12-07 23:28:00 -0600

    added resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import './index.scss';
-
+import ResumePic from './assets/Resume.png'
 
 const Resume = () => {
     return (
         <div className='container resume-page'>
-
+               <div className="text-zone">
+                <h1>
+                Resume:
+                </h1>
+                <img className="resume-pic" src={ResumePic} alt="resume" />
+                <p className='resume-text'>
+                <a target="_blank" href="https://docs.google.com/document/d/1R44nT2pZIbezYDaMGEfk4w5MxDnjzurmetr_6bz7G9U/edit?usp=sharing" className="resume-link">Click here to download</a>
+                </p>
+            </div>
         </div>
     )
 }
