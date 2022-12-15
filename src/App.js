8438fd37b273717050d2e7cd36ff9085665a9b67
Brandon Knight <knightbrandon1984@gmail.com>
2022-12-14 22:30:49 -0600

    added redirection

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="portfolio" element={<Portfolio />} />
-        <Route path="resume" element={<Resume />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/resume" element={<Resume />} />
         </Route>
     </Routes>
     </>
