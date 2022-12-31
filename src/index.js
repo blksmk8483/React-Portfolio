4f53ebdfc3ddb65171d2d5d816ded961129fb3ab
Brandon Knight <knightbrandon1984@gmail.com>
2022-12-31 13:21:52 -0600

    syntax trial and error

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename='/React-Portfolio/'>
+    <HashRouter basename='/React-Portfolio'>
     <App />
     </HashRouter>
   </React.StrictMode>,
