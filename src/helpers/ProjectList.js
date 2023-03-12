f6270d333946ef0e6d1f912e0a351f70f26b8108
Brandon Knight <knightbrandon1984@gmail.com>
2023-03-11 20:50:02 -0600

    added more the profile

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,7 +1,11 @@
 import ClashOfCharacters from '../assets/portfolio/ClashOfCharacters.png';
-import Garden2 from '../assets/portfolio/gardenlogo.png';
+import Garden2 from '../assets/portfolio/screenPlay.gif';
 import ECommerce from '../assets/portfolio/E-Commerce-Site.png';
 import TeamProfile from '../assets/portfolio/Team-Profile-Generator.png';
+import EmployeeTracker from '../assets/portfolio/Employee-tracker.png';
+import Journal from '../assets/portfolio/screen_recording.gif';
+import SocialNetwork from '../assets/portfolio/Social-Network-API.png';
+import TechBlog from '../assets/portfolio/Tech-Blog.png';
 
 
 export const ProjectList = [
@@ -25,16 +29,20 @@ export const ProjectList = [
         image: TeamProfile,
     },
     {
-        name: "Garden 2",
-        image: Garden2,
+        name: "Employee Tracker",
+        image: EmployeeTracker,
     },
     {
-        name: "Garden 2",
-        image: Garden2,
+        name: "Journal Playlist",
+        image: Journal,
     },
     {
-        name: "Garden 2",
-        image: Garden2,
+        name: "Social Network",
+        image: SocialNetwork,
+    },
+    {
+        name: "Tech-Blog",
+        image: TechBlog,
     },
     
 ]
