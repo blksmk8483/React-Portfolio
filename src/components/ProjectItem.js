d9638ec4c095d83c1172421b9dccaa99bb3cd43a
Brandon Knight <knightbrandon1984@gmail.com>
2023-03-11 23:19:58 -0600

    updated styling on Personal Projects

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -7,21 +7,20 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 function ProjectItem({ image, name, id, link }) {
     const navigate = useNavigate();
 
+    const handleChange = () => {
+        navigate("/project/" + id);
+    };
+
     const projectLink = <a target="_blank" rel='nonreferrer' href={link}>
     <FontAwesomeIcon icon={faGithub} />
 </a>
 
     return (
         <div className="projectItem" >
-        <div 
-        
-        onClick={() => {navigate("/project/" + id);
-        }}
-        >
-            <div style={{backgroundImage: `url(${image})`}} className='bgImage' />
+       
+            <div onClick={handleChange} style={{backgroundImage: `url(${image})`}} className='bgImage' />
             <h1>{name}</h1>
-            </div>
-            <div className="projectLink">{projectLink}</div>
+            <h2 className='projectLink'>{projectLink}</h2>
         </div>
     );
 };
